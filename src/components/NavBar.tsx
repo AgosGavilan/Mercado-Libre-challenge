6422f97810beccd166c679befd935ead751fd7e3
Agostina Gavilan <agosgavilan3@gmail.com>
2022-07-28 23:04:43 -0300

    probando font family

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -58,7 +58,7 @@ export const NavBar: React.FC = () => {
             maxWidth={600}
             width="100%"
           >
-            <Input variant="unstyled" paddingX={2} placeholder="Busca productos, marcas y más..." _placeholder={{color: "blackAlpha.400"}}/>
+            <Input variant="unstyled" paddingX={2} placeholder="Busca productos, marcas y más..." _placeholder={{color: "blackAlpha.400", fontFamily: "body"}}/>
             <Icon as={AiOutlineSearch} width={5} height={5} color="gray.400" cursor="pointer"></Icon>{/*lupita de la Navbar */}
           </Stack>
         </Stack>
@@ -81,10 +81,10 @@ export const NavBar: React.FC = () => {
               alignSelf="normal"
             />
             <Stack spacing={-1}>
-              <Text color="blackAlpha.600" fontSize="xs" lineHeight="normal">
+              <Text color="blackAlpha.600" fontSize="xs" lineHeight="normal" fontFamily="body">
                 Enviar a
               </Text>
-              <Text lineHeight="normal" fontSize="sm">
+              <Text lineHeight="normal" fontSize="sm" fontFamily="body">
                 Formosa 3600
               </Text>
             </Stack>
@@ -98,7 +98,7 @@ export const NavBar: React.FC = () => {
             <Menu>
               <MenuButton _hover={{color: "blackAlpha.800"}}>
                 <Stack direction="row" alignItems="center" spacing={1}>
-                  <Text>Categorías</Text>
+                  <Text fontFamily="body">Categorías</Text>
                   <BiChevronDown/>
                 </Stack>
               </MenuButton>
@@ -108,18 +108,18 @@ export const NavBar: React.FC = () => {
                 ))}
               </MenuList>
             </Menu>
-            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}}>Ofertas</Text>
-            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}}>Historial</Text>
-            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}}>Supermercado</Text>
-            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}}>Moda</Text>
-            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}}>Vender</Text>
-            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}}>Ayuda</Text>
+            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}} fontFamily="body">Ofertas</Text>
+            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}} fontFamily="body">Historial</Text>
+            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}} fontFamily="body">Supermercado</Text>
+            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}} fontFamily="body">Moda</Text>
+            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}} fontFamily="body">Vender</Text>
+            <Text cursor="pointer" _hover={{color: "blackAlpha.800"}} fontFamily="body">Ayuda</Text>
           </Stack>
         </Stack>
         <Stack direction="row" spacing={5} fontSize="sm" alignItems="center">
-          <Text cursor="pointer">Creá tu cuenta</Text>
-          <Text cursor="pointer">Ingresá</Text>
-          <Text cursor="pointer">Mis compras</Text>
+          <Text cursor="pointer" fontFamily="body">Creá tu cuenta</Text>
+          <Text cursor="pointer" fontFamily="body">Ingresá</Text>
+          <Text cursor="pointer" fontFamily="body">Mis compras</Text>
           <Icon as={IoCartOutline} height={6} width={6} cursor="pointer"></Icon>
         </Stack>
       </Stack>
